ceabe8690a55eb7535c99f2877f2cc3ce1dbb86c
ahmad-alkadri <ahmad.alkadri@outlook.com>
2023-08-20 01:49:50 +0200

    Add another test

--- reverie/backend_server/test_embedding.py
+++ reverie/backend_server/test_embedding.py
@@ -1,8 +1,8 @@
 import openai
-from gpt4all import GPT4All, Embed4All
-from utils import *
+from gpt4all import Embed4All
+from utils import openai_api_key
 
-def openai_get_embedding(text, model="text-embedding-ada-002"):
+def openai_get_embedding(text: str, model: str="text-embedding-ada-002") -> list:
 	openai.api_key = openai_api_key
 	text = text.replace("\n", " ")
 	print(text)
@@ -10,7 +10,7 @@ def openai_get_embedding(text, model="text-embedding-ada-002"):
 		text = "this is blank"
 	return openai.Embedding.create(input=[text], model=model)["data"][0]["embedding"]
 
-def loc_get_embedding(text):
+def loc_get_embedding(text: str) -> list:
 	text = text.replace("\n", " ")
 	embedder = Embed4All()
 	output = embedder.embed(text)
